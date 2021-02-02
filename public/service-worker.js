9ed61f21a9c0b80fdf2a4d1c5cab7cf1d8444b4f
Luke Martin <ljmart02@gmail.com>
2021-02-01 19:32:18 -0700

    Updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,15 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
 "/",
 "/index.html",
 "/index.js",
-"/style.css",
+"/styles.css",
 "/routes/api.js",
-"/icons/icon-192x192.png",
-"/icons/icon-512x512.png"
-]
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+"./icons/icon-192x192.png",
+"./icons/icon-512x512.png"
+];
 
 // install
 self.addEventListener("install", function(evt) {

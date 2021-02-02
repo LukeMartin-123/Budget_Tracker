1c4343a618ac35413b05d26e0274282f43a60a79
Luke Martin <ljmart02@gmail.com>
2021-02-01 20:01:59 -0700

    Fixed bug

--- public/database.js
+++ public/database.js
@@ -1,56 +1,56 @@
 const request = indexedDB.open("budgetDatabase", 1);
 
-    request.onupgradeneeded = event => {
-      const db = event.target.result;
+request.onupgradeneeded = event => {
+    const db = event.target.result;
 
-      db.createObjectStore("pending", { autoIncrement: true });
-    };
+    db.createObjectStore("pending", { autoIncrement: true });
+};
 
-    request.onerror = (event) => {
-      console.log("Oh no! " + event.target.errorCode);
-    }
+request.onerror = (event) => {
+    console.log("Oh no! " + event.target.errorCode);
+}
 
 
-    request.onsuccess = ({ target }) => {
-      db = target.result;
+request.onsuccess = ({ target }) => {
+    db = target.result;
 
-      if (navigator.onLine) {
+    if (navigator.onLine) {
         checkDatabase();
-      }
-    };
-
-    function saveRecord(record) {
-      const transaction = db.transaction(["pending"], "readwrite");
-      const store = transaction.objectStore("pending");
-
-      store.add(record);
     }
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
 
 
-    function checkDatabase() {
-      const transaction = db.transaction(["pending"], "readwrite");
-      const store = transaction.objectStore("pending");
-      const getAll = store.getAll();
-      
-      getAll.onsuccess = function() {
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
         if (getAll.result.length > 0) {
-          fetch("/api/transaction/bulk", {
-            method: "POST",
-            body: JSON.stringify(getAll.result),
-            headers: {
-              Accept: "application/json, text/plain, */*",
-              "Content-Type": "application/json"
-            }
-          })
-            .then(response => {
-              return response.json();
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
             })
-            .then(() => {
-              // delete records if successful
-              const transaction = db.transaction(["pending"], "readwrite");
-              const budgetStore = transaction.objectStore("pending");
-              budgetStore.clear();
-            });
+                .then(response => {
+                    return response.json();
+                })
+                .then(() => {
+                    // delete records if successful
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const budgetStore = transaction.objectStore("pending");
+                    budgetStore.clear();
+                });
         }
-      };
-    }
+    };
+}
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,11 @@
 var CACHE_NAME = "static-cache-v2";
-var DATA_CACHE_NAME = "data-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
 "/",
 "/index.html",
 "/index.js",
 "/styles.css",
-"/routes/api.js",
 "/icons/icon-192x192.png",
 "/icons/icon-512x512.png"
 ];
